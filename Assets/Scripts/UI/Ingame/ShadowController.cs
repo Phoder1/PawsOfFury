33cd3fbfe1d62feb97e107c9885a10b906096a50
Alon Talmi <35352105+Phoder1@users.noreply.github.com>
2021-04-24 11:10:13 +0300

    Shadow fix

--- Assets/Scripts/UI/Ingame/ShadowController.cs
+++ Assets/Scripts/UI/Ingame/ShadowController.cs
@@ -1,30 +1,25 @@
-using CustomAttributes;
-using Sirenix.OdinInspector;
 using UnityEngine;
 
 public class ShadowController : MonoBehaviour
 {
-    //[LocalComponent(false, true)]
-    //[SerializeField] ParticleSystem particleSystem;
-    [LocalComponent(hideProperty: false, getComponentFromChildrens: true)]
-    [SerializeField] ParticleSystem particles;
+    [SerializeField] Material pulsatingMaterial;
     [SerializeField] Color color;
     public Color Color
     {
-        get => particles.main.startColor.color;
+        get => pulsatingMaterial.color;
         set
         {
-            var main = particles.main.startColor;
-            main.color = value;
+            if (Color != value)
+                pulsatingMaterial.color = value;
         }
     }
-    public void ChangeColor(Color color)
+    public void ChangeColor()
     {
         Color = color;
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
-            ChangeColor(color);
+            ChangeColor();
     }
 }
