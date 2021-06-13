5126dd258725d8093fb358bf329e82d378bda424
Alon Talmi <35352105+Phoder1@users.noreply.github.com>
2021-06-13 09:25:36 +0300

    Minion book

--- Assets/Scripts/ScriptableObjects/UnitAssetsSO.cs
+++ Assets/Scripts/ScriptableObjects/UnitAssetsSO.cs
@@ -1,11 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 [CreateAssetMenu(menuName = "ScriptableObjects/UnitAssets")]
 public class UnitAssetsSO : ScriptableObject
 {
     [SerializeField]
-    private Sprite[] _tierCrystals;
-    public Sprite[] TierCrystals => _tierCrystals;
+    private TierAsset[] _tierAssets = new TierAsset[3];
+    public TierAsset[] TierAssets => _tierAssets;
+
+    [Serializable]
+    public class TierAsset
+    {
+    [SerializeField]
+    private Sprite _crystalSprite;
+    public Sprite CrystalSprite => _crystalSprite;
+
+    [SerializeField]
+    private Color _borderColor;
+    public Color BorderColor => _borderColor;
+    [SerializeField]
+    private Color _backgroundColor;
+    public Color BackgroundColor => _borderColor;
+        
+    }
 }
