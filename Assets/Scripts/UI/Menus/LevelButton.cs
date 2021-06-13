10abaf6501b3acfa9e020e4040301f63134571a7
sahar <sahar.malter1@gmail.com>
2021-06-13 09:53:12 +0300

    colors and transitions

--- Assets/Scripts/UI/Menus/LevelButton.cs
+++ Assets/Scripts/UI/Menus/LevelButton.cs
@@ -1,22 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AddressableAssets;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.AddressableAssets;
 
-public class LevelButton : MonoBehaviour
-{
-    [SerializeField]
-    private AssetReference _levelRefrence;
+//public class LevelButton : MonoBehaviour
+//{
+//    [SerializeField]
+//    private AssetReference _levelRefrence;
 
-    private SceneLoader sceneLoader;
+//    private SceneLoader sceneLoader;
 
-    private void Awake()
-    {
-        sceneLoader = GetComponentInParent<SceneLoader>();
-    }
-    public void LoadScene() => sceneLoader.LoadScene(_levelRefrence);
-    private void OnValidate()
-    {
-        Debug.Log(_levelRefrence.Asset?.GetType());
-    }
-}
+//    private void Awake()
+//    {
+//        sceneLoader = GetComponentInParent<SceneLoader>();
+//    }
+//  //  public void LoadScene() => sceneLoader.LoadScene(_levelRefrence);
+//    private void OnValidate()
+//    {
+//        Debug.Log(_levelRefrence.Asset?.GetType());
+//    }
+//}
