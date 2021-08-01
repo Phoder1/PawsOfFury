f939db6dc5cb9e1a9b6c38311a1fc6aac271717e
Alon Talmi <35352105+Phoder1@users.noreply.github.com>
2021-08-01 23:50:23 +0300

    Popup window

---
+++ Assets/Scripts/animation/TweenSequenceManager.cs
@@ -0,0 +1,68 @@
+using DG.Tweening;
+using Sirenix.OdinInspector;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[DefaultExecutionOrder(1)]
+public class TweenSequenceManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<SequnceTween> _tweenAnimations = default;
+
+    [SerializeField, FoldoutGroup("Events")]
+    private UnityEvent OnComplete = default;
+    [SerializeField, FoldoutGroup("Events")]
+    private UnityEvent OnRewind = default;
+    Sequence _sequence = default;
+    private void Start()
+    {
+        _sequence = DOTween.Sequence();
+        _sequence.SetAutoKill(false);
+        _sequence.onRewind += OnWindowClose;
+        _sequence.onComplete += OnWindowOpen;
+        foreach (var tweenAnimation in _tweenAnimations)
+        {
+            Tween tween = tweenAnimation.tweenAnimation.tween;
+            if (tween != null)
+                _sequence.Insert(tweenAnimation.delay, tween);
+        }
+    }
+    private void OnDestroy()
+    {
+        if (_sequence.IsActive())
+            _sequence.Kill();
+    }
+    public void PlayForward()
+    {
+        _sequence.PlayForward();
+    }
+    public void PlayBackwards()
+    {
+        _sequence.PlayBackwards();
+    }
+    public void Pause()
+    {
+        _sequence.Pause();
+    }
+    public void Play()
+    {
+        _sequence.Play();
+    }
+    void OnWindowOpen()
+    {
+        OnComplete?.Invoke();
+    }
+    void OnWindowClose()
+    {
+        OnRewind?.Invoke();
+    }
+
+    [Serializable, InlineProperty]
+    private class SequnceTween
+    {
+        public DOTweenAnimation tweenAnimation = null;
+        public float delay = 0;
+    }
+}
